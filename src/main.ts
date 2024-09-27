5d83124a4909d62154ab9190a5a26a769c72c652
Anatoly Politsin <politsin@gmail.com>
2024-09-27 16:24:33 +0300

    rightSidebar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { Notice, Plugin } from "obsidian";
+import { Notice, Plugin, WorkspaceLeaf } from "obsidian";
 import { AiEngineerSettingTab } from "./set/settings";
 import { AiSettings, DEFAULTS } from "./set/settingsType";
+import { AiEngineerChatView, VIEW_TYPE_AI_CHAT } from "./view/chat";
 
 // Remember to rename these classes and interfaces!
 
@@ -16,7 +17,8 @@ export default class AiEngineer extends Plugin {
       "Ai Engineer",
       (evt: MouseEvent) => {
         // Called when the user clicks the icon.
-        new Notice("This is a notice!");
+        new Notice("Ai Chat tab open!");
+        this.activateView();
       }
     );
     // Perform additional things with the ribbon
@@ -28,6 +30,11 @@ export default class AiEngineer extends Plugin {
 
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new AiEngineerSettingTab(this.app, this));
+
+    this.registerView(
+      VIEW_TYPE_AI_CHAT,
+      (leaf) => new AiEngineerChatView(leaf)
+    );
   }
 
   async loadSettings() {
@@ -36,5 +43,32 @@ export default class AiEngineer extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
+  }
+
+  async activateView() {
+    const { workspace } = this.app;
+
+    let leaf: WorkspaceLeaf | null = null;
+    const leaves = workspace.getLeavesOfType(VIEW_TYPE_AI_CHAT);
+
+    if (leaves.length > 0) {
+      // A leaf with our view already exists, use that
+      leaf = leaves[0];
+    } else {
+      // Our view could not be found in the workspace, create a new leaf
+      // in the right sidebar for it
+      leaf = workspace.getRightLeaf(false);
+      if (leaf) {
+        await leaf.setViewState({ type: VIEW_TYPE_AI_CHAT, active: true });
+      } else {
+        console.error("Could not obtain a WorkspaceLeaf.");
+        new Notice("Error: Could not open Ai Chat tab.");
+      }
+    }
+
+    // "Reveal" the leaf in case it is in a collapsed sidebar
+    if (leaf) {
+      workspace.revealLeaf(leaf);
+    }
   }
 }
